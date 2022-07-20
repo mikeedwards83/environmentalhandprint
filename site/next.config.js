a1b62eda777196ba8ab3cb8e07ea17191203ccbe
mikeedwards83 <mike@glass.lu>
2022-07-20 23:03:01 +0100

    attempting webpack fix for build

--- site/next.config.js
+++ site/next.config.js
@@ -1,5 +1,17 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  
+  webpack: (config, options) => {
+    if (!options.dev && options.isServer) {
+      const originalEntry = config.entry
+
+      config.entry = async () => {
+        const entries = { ...(await originalEntry()) }
+        entries['scripts/build-rss'] = './scripts/build-rss.js'
+        return entries
+      }
+    }
+
+    return config;
+  }
 }
